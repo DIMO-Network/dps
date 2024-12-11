7d18dcb396a123970fb460d8ab707bdaa3271440
dmytrozakharenko <amidum@gmail.com>
2024-12-11 11:14:29 -0500

    - added back  dimo_split_values processor - added more make targets

---
+++ internal/processors/splitvalues/splitvalues.go
@@ -0,0 +1,51 @@
+package splitvalues
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/redpanda-data/benthos/v4/public/service"
+)
+
+const CloudEventIndexValueKey = "dimo_cloudevent_index_value"
+
+var configSpec = service.NewConfigSpec().
+	Summary("Pulls index values off a cloudevent message and returns seperate messagaes for each field")
+
+func init() {
+	err := service.RegisterBatchProcessor("dimo_split_values", configSpec, ctor)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func ctor(cfg *service.ParsedConfig, mgr *service.Resources) (service.BatchProcessor, error) {
+	return processor{}, nil
+}
+
+type processor struct{}
+
+// Close to fulfill the service.Processor interface.
+func (processor) Close(context.Context) error { return nil }
+
+// ProcessBatch to fulfill the service.Processor sets an error on each message.
+func (p processor) ProcessBatch(_ context.Context, msgs service.MessageBatch) ([]service.MessageBatch, error) {
+	var retMsgs []*service.Message
+	for _, msg := range msgs {
+		values, ok := msg.MetaGetMut(CloudEventIndexValueKey)
+		if !ok {
+			return nil, errors.New("no index values found")
+		}
+		valSlice, ok := values.([][]any)
+		if !ok {
+			return nil, fmt.Errorf("index values is not a slice of slices instead is %T", values)
+		}
+		for _, vals := range valSlice {
+			newMsg := msg.Copy()
+			newMsg.SetStructured(vals)
+			retMsgs = append(retMsgs, newMsg)
+		}
+	}
+	return []service.MessageBatch{retMsgs}, nil
+}
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+
+	"github.com/redpanda-data/benthos/v4/public/service"
+
+	// Import aws for s3 output.
+	_ "github.com/redpanda-data/connect/v4/public/components/aws"
+
+	// Import sql for clickhouse output.
+	_ "github.com/redpanda-data/connect/v4/public/components/sql"
+
+	// Import io for http endpoints.
+	_ "github.com/redpanda-data/connect/v4/public/components/io"
+
+	// Import pure for basic processing.
+	_ "github.com/redpanda-data/benthos/v4/public/components/pure"
+
+	// Import prometheus for metrics.
+	_ "github.com/redpanda-data/connect/v4/public/components/prometheus"
+
+	// Add our custom plugin packages here.
+	_ "github.com/DIMO-Network/dps/internal/processors/splitvalues"
+)
+
+func main() {
+	service.RunCLI(context.Background())
+}
